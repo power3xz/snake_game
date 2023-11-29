ebd56bd9188932820fc7fe4adbdb0edffc3a18e4
devj <power3xz@gmail.com>
2023-11-29 16:54:41 +0900

    wasm data 추가

--- www/index.js
+++ www/index.js
@@ -12,7 +12,10 @@ async function init() {
   const response = await fetch("./sum.wasm");
   const buffer = await response.arrayBuffer();
   const wasm = await WebAssembly.instantiate(buffer, importObject);
+
   const sumFunction = wasm.instance.exports.sum;
+  const wasmMemory = wasm.instance.exports.mem;
+  debugger;
   const result = sumFunction(10, 50);
   console.log(result);
 }
