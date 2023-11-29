6878026a552f09dbdcd925b815c9abcb4b3236a9
devj <power3xz@gmail.com>
2023-11-29 16:44:44 +0900

    객체 임포트 추가

--- www/index.js
+++ www/index.js
@@ -1,7 +1,17 @@
 async function init() {
+  const importObject = {
+    console: {
+      log: () => {
+        console.log("Just logging Something!");
+      },
+      error: () => {
+        console.log("I am just error");
+      },
+    },
+  };
   const response = await fetch("./sum.wasm");
   const buffer = await response.arrayBuffer();
-  const wasm = await WebAssembly.instantiate(buffer);
+  const wasm = await WebAssembly.instantiate(buffer, importObject);
   const sumFunction = wasm.instance.exports.sum;
   const result = sumFunction(10, 50);
   console.log(result);
