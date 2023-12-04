4b8a43741addc5024bbbdffe26c9a338352b4b3b
devj <power3xz@gmail.com>
2023-12-04 18:28:17 +0900

    alert 추가

--- www/index.js
+++ www/index.js
@@ -1,25 +1,5 @@
-async function init() {
-  const memory = new WebAssembly.Memory({ initial: 1 });
-  const importObject = {
-    js: {
-      mem: memory,
-    },
-    console: {
-      log: () => {
-        console.log("Just logging Something!");
-      },
-      error: () => {
-        console.log("I am just error");
-      },
-    },
-  };
-  const response = await fetch("./sum.wasm");
-  const buffer = await response.arrayBuffer();
-  await WebAssembly.instantiate(buffer, importObject);
+import init, { greet } from "snake_game";
 
-  debugger;
-  const uint8Array = new Uint8Array(memory.buffer, 0, 2);
-  const hiText = new TextDecoder().decode(uint8Array);
-  console.log(hiText);
-}
-init();
+init().then((wasm) => {
+  greet("good");
+});
