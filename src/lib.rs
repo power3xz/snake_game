95ad5d7531ac963b31d2cafd094ddc308e5c8d4c
devj <power3xz@gmail.com>
2023-12-05 13:43:36 +0900

    방향 추가

--- src/lib.rs
+++ src/lib.rs
@@ -5,16 +5,26 @@ use wee_alloc::WeeAlloc;
 #[global_allocator]
 static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
+#[derive(PartialEq)]
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
 struct SnakeCell(usize);
 
 struct Snake {
     body: Vec<SnakeCell>,
+    direction: Direction,
 }
 
 impl Snake {
     fn new(spawn_index: usize) -> Snake {
         Snake {
             body: vec![SnakeCell(spawn_index)],
+            direction: Direction::Down,
         }
     }
 }
@@ -46,6 +56,22 @@ impl World {
 
     pub fn update(&mut self) {
         let snake_idx = self.snake_head_idx();
-        self.snake.body[0].0 = (snake_idx + 1) % self.size;
+        let row = snake_idx / self.width;
+
+        if self.snake.direction == Direction::Right {
+            let next_col = (snake_idx + 1) % self.width;
+            self.snake.body[0].0 = row * self.width + next_col;
+        }
+
+        if self.snake.direction == Direction::Left {
+            let next_col = (snake_idx - 1) % self.width;
+            self.snake.body[0].0 = row * self.width + next_col;
+        }
+        if self.snake.direction == Direction::Up {
+            self.snake.body[0].0 = (snake_idx - self.width) % self.size;
+        }
+        if self.snake.direction == Direction::Down {
+            self.snake.body[0].0 = (snake_idx + self.width) % self.size;
+        }
     }
 }
